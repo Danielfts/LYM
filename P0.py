84edfcc7bd14dc04a4bb492eba373339baf8eb5c
Danielfts <d.trivino@uniandes.edu.co>
2023-02-01 18:24:14 -0500

    minor changes

--- P0.py
+++ P0.py
@@ -1,3 +1,22 @@
 """ 
 Proyecto P0 de lenguajes y máquinas 
 """
+import re
+
+noTerminales = ["vars", "procs"]
+terminales = [",", ";", "|", "[","]", ":",
+    "robot_r"
+]
+
+def openCode(file):
+    data = ''
+    f = open(file,'r')
+    for i in f:
+        #i = re.sub(' ',"",string=i) 
+        i = i.lower()
+        data += i
+    print(data)
+
+    return data
+
+openCode('code.txt')
