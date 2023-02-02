29ed0a6dff80463d41fbcf0a231b7a46e461c890
Danielfts <d.trivino@uniandes.edu.co>
2023-02-01 19:41:20 -0500

    base

--- P0.py
+++ P0.py
@@ -3,20 +3,44 @@ Proyecto P0 de lenguajes y máquinas
 """
 import re
 
-noTerminales = ["vars", "procs"]
-terminales = [",", ";", "|", "[","]", ":",
-    "robot_r"
-]
+noTerminales = []
+terminales = []
+parseTree= {
+
+}
+
+def loadLang(file):
+    global terminales
+    f = open(file, 'r')
+    for i in f:
+        terminales.append(i.strip())
+    #print(terminales)     
+    f.close()
 
 def openCode(file):
     data = ''
     f = open(file,'r')
     for i in f:
-        #i = re.sub(' ',"",string=i) 
+        i = re.sub(',',' , ',i)
+        i = re.sub('\n'," ",string=i) 
         i = i.lower()
         data += i
-    print(data)
-
+    data = data.split()
+    print(tuple(data))
+    f.close()
     return data
 
-openCode('code.txt')
+def tokenizer(data:list)->bool:
+    result = False
+    if data[0] == 'robot_r':
+
+        pass
+    else:
+        result = False
+    return result
+
+def main():
+    loadLang('lang.txt')
+    openCode('code.txt')
+
+main()
