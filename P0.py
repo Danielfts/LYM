36b28f7d0b884b959ff04545a26700e0ac3d094d
dcamachom <D.CAMACHOM@UNIANDES.EDU.CO>
2023-02-06 19:53:29 -0500

    function to recognize conditionals

--- P0.py
+++ P0.py
@@ -58,7 +58,7 @@ def searchItem(var:str)->bool:
             return True
     return False
 
-#FUNCIONES PARA IDENTIFICAR COMANDOS
+#FUNCIONES PARA VERIFICAR COMANDOS
 
 def checkAssignTo(function:list)-> bool:
     #Lista de tamaño 4
@@ -66,9 +66,9 @@ def checkAssignTo(function:list)-> bool:
     if len(function)!=4:
         return False
     else:
-        if function[0]==":" and type(function[1])=="int" and type(function[2])=="str":
+        if function[0]==":" and type(function[1])=="int" and type(function[3])=="str" and  function[2]==",":
             return True
-        elif function[0]==":" and type(function[2])=="str" and searchItem(function[1]):
+        elif function[0]==":" and type(function[3])=="str" and searchItem(function[1]) and function[2]==",":
             return True
         else:
             return False
@@ -80,9 +80,9 @@ def checkGoto (function: list)->bool:
         return False
     else:
         #revisar variables----
-        if function[0]==":" and type(function[1])=="int" and type(function[2])=="int":
+        if function[0]==":" and type(function[1])=="int" and type(function[3])=="int" and function[2]==",":
             return True
-        elif function[0]==":" and searchItem(function[1]) and type(function[2])=="int":
+        elif function[0]==":" and searchItem(function[1]) and type(function[3])=="int" and function[2]==",":
             return True
         else:
             return False
@@ -128,9 +128,9 @@ def checkPutOrPick (function: list)-> bool:
     if len(function)!=4:
         return False
     else:
-        if function[0]==":" and type(function[1])=="int" and (function[2]=="balloons" or function[2]=="chips"):
+        if function[0]==":" and type(function[1])=="int" and (function[3]=="balloons" or function[3]=="chips") and function[2]==",":
             return True
-        elif function[0]==":" and searchItem(function[1]) and (function[2]=="balloons" or function[2]=="chips"):
+        elif function[0]==":" and searchItem(function[1]) and (function[3]=="balloons" or function[3]=="chips") and function[2]==",":
             return True
         else:
             return False
@@ -141,11 +141,11 @@ def checkMoveToOrJumpTo (function: list) -> bool:
     if len(function)!=4:
         return False
     else:
-        if function[0]==":" and type(function[1])=="int" and ((function[2]=="front")
-            or function[2]=="back" or function[2]=="right" or function[2]=="left"):
+        if function[0]==":" and type(function[1])=="int" and ((function[3]=="front")
+            or function[3]=="back" or function[3]=="right" or function[3]=="left") and function[2]==",":
             return True
-        elif function[0]==":"  and searchItem(function[1]) and ((function[2]=="front")
-            or function[2]=="back" or function[2]=="right" or function[2]=="left"):
+        elif function[0]==":"  and searchItem(function[1]) and ((function[3]=="front")
+            or function[3]=="back" or function[3]=="right" or function[3]=="left") and  function[2]==",":
             return True
         else:
             return False
@@ -156,11 +156,11 @@ def checkMoveOrjumpIndir (function: list)->bool:
     if len(function)!=4:
         return False
     else:
-        if function[0]==":" and type(function[1])=="int" and (function[2]=="south" or function[2]=="north" or
-            function[2]=="east" or function[2]=="west"):
+        if function[0]==":" and type(function[1])=="int" and (function[3]=="south" or function[3]=="north" or
+            function[3]=="east" or function[3]=="west") and function[2]==",":
             return True
         elif function[0]==":" and searchItem(function[1]) and (function[2]=="south" or function[2]=="north" or
-            function[2]=="east" or function[2]=="west"):
+            function[2]=="east" or function[2]=="west") and function[2]==",":
             return True
         else:
             return False
@@ -200,6 +200,79 @@ def recognizeCommand (command: str, function:list)->bool:
         recognized=chechNop
     return recognized
 
+#FUNCIONES PARA VERIFICAR CONDICIONALES
+
+def checkFacing(function: list)->bool:
+    #Lista de tamaño 2
+    #facing: O
+    if len(function)!=2:
+        return False
+    elif function[0]==":" and (function[1]=="north" or function[1]=="south" or function[1]=="west" or function[1]=="east"):
+        return True
+    else:
+        return False
+
+def checkCanPutPick(function:list)->bool:
+    #Lista de tamaño 4
+    #canput/pick: n,x
+    if len(function)!=4:
+        return False
+    elif function[0]==":" and (type(function[1]))=="int" and function[2]=="," and (function[3]=="balloons" or function[3]=="chips"):
+        return True
+    else:
+        return False
+
+def checkCanMoveJumpInDir (function:list)->bool:
+    #Lista de tamaño 4
+    #canmove/jumpindir: n,D
+    if len(function)!=4:
+        return False
+    elif function[0]==":" and (type(function[1]))=="int" and function[2]=="," and  (function[3]=="north" and function[3]=="south"
+        and function[3]=="west" and function[3]=="east"):
+        return True
+    else:
+        return False
+
+def checkCanMoveJumpTo (function:list)->bool:
+    #Lista de tamaño 4
+    #canMove/jumptothe: n,o
+    if len(function)!=4:
+        return False
+    elif function[0]==":" and (type(function[1]))=="int" and function[2]=="," and (function[3]=="front" and function[3]=="right" and
+        function[3]=="back" and function[3]=="left"):
+        return True
+    else:
+        return False
+
+def checkNot(function: list, command:list)->bool:
+    #Lista de tamaño 2
+    #not: cond
+    if len(function):
+        return False
+    elif function[0]==":" and recognizeCommand(function[1], command):
+        return True
+    else:
+        return False
+
+#IDENTIFICAR USO DE CONDICIONALES
+
+def recognizeConditional (cond:str, function: list)->bool:
+    recognized=False
+    if cond=="facing":
+        recognized=checkFacing(function)
+    elif cond=="canput" or cond=="canpick":
+        recognized=checkCanPutPick(function)
+    elif cond=="canmoveindir" or cond=="canjumpindir":
+        recognized=checkCanMoveJumpInDir(function)
+    elif cond=="canmovetothe" or cond=="canjumptothe":
+        recognized=checkCanMoveJumpTo(function)
+    return recognized
+
+def recognizeNot (cond: str, function:list, command,list)->bool:
+    if cond=="not":
+        return checkNot(function,command)
+    else:
+        return False
 
 #Main
 
